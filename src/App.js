a399282301d8a81b1addb5fe306783fe4c3ee0ba
Judith van Leersum <judithvleersum@gmail.com>
2022-07-23 12:29:42 +0200

    Created basic pages with layout and navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Quotes from './pages/Quotes';
 import QuoteDetails from './pages/QuoteDetails';
 import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
         <Route path='/quotes' exact>
           <Quotes />
         </Route>
@@ -18,7 +22,7 @@ function App() {
           <NewQuote />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from '../components/comments/Comments';
+
 const QuoteDetails = () => {
-  return <h1>Quote Details</h1>
+  const params = useParams();
+
+  return <Fragment>
+      <h1>Quote Details for quote {params.quoteId}</h1>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
 }
 
 export default QuoteDetails;
