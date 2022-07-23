785f5ef78c75398375463ecb1a4c1608f3f8db7a
Judith van Leersum <judithvleersum@gmail.com>
2022-07-23 12:12:48 +0200

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import Quotes from './pages/Quotes';
+import QuoteDetails from './pages/QuoteDetails';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path='/quotes' exact>
+          <Quotes />
+        </Route>
+        <Route path='/quotes/:quoteId' >
+          <QuoteDetails />
+        </Route>
+        <Route path='/new-quote' exact>
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
