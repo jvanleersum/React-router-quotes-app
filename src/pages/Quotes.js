280e11a1f58ef2ad9fa87c9499962ec74e03c0b8
Judith van Leersum <judithvleersum@gmail.com>
2022-07-23 13:24:57 +0200

    Linked all quotes and quote detail page, using dummy data

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,5 +1,25 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Judith', 
+    text: 'React is the best'
+  },
+  {
+    id: 'q2',
+    author: 'Max',
+    text: "The Other Option is the best"
+  },
+  {
+    id: "q3",
+    author: 'Taki',
+    text: 'Food is the best',
+  }
+]
+
 const Quotes = () => {
-  return <h1>All Quotes</h1>
+  return <QuoteList quotes={DUMMY_QUOTES}/>
 }
 
 export default Quotes;
