5c96c44cd72a2d06f33de9e2ada98a0daf66d125
Judith van Leersum <judithvleersum@gmail.com>
2022-07-25 18:12:31 +0200

    Added functionality to add quotes

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,40 +1,41 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'Judith', 
-    text: 'React is the best'
-  },
-  {
-    id: 'q2',
-    author: 'Max',
-    text: "The Other Option is the best"
-  },
-  {
-    id: "q3",
-    author: 'Taki',
-    text: 'Food is the best',
-  }
-]
+import LoadingSpinner from "../components/ui/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllComments, getSingleQuote } from "../lib/api";
 
 const QuoteDetails = () => {
-  const params = useParams();
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
-
   const match = useRouteMatch();
+  const params = useParams();
+  const { quoteId } = params;
 
-  if (!quote) {
+  const {sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true)
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId])
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
